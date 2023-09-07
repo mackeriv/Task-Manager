495a0d36aafd8d9eb4e89157e4e37d0e4bbaca67
mackeriv <mackeriv@gmail.com>
2023-09-07 15:19:32 -0300

    Drafting property classes for user, task.

---
+++ src/main/java/com/example/taskmgr/User.java
@@ -0,0 +1,42 @@
+package com.example.taskmgr;
+
+public class User {
+
+    int id;
+    String username;
+    int password;
+
+    public User(int id, String username, int password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+        // Default constructor
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+}
