ac369f0452abfc724307d2fd1fad2aaf3dcd8d0e
mackeriv <mackeriv@gmail.com>
2023-09-08 15:21:44 -0300

    Drafting a skeleton of classes.

--- src/main/java/com/example/taskmgr/TaskController.java
+++ src/main/java/com/example/taskmgr/TaskController.java
@@ -3,6 +3,7 @@ package com.example.taskmgr;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TaskController {
+
     @Autowired
     private TaskService taskService;
 
